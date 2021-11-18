f0c08648f9a00f0be7db964345e83eebed6d3cab
liqijian <li.qijian@scistor.com>
2021-11-18 11:24:25 +0800

    分割符可以自定义了

--- src/main/java/com/wxscistor/jsonhandle/JsonUtils.java
+++ src/main/java/com/wxscistor/jsonhandle/JsonUtils.java
@@ -23,18 +23,18 @@ public class JsonUtils {
      * @param fileUrl
      * @throws IOException
      */
-    public static void jsonHandle(String fileUrl) throws IOException {
+    public static void jsonHandle(String fileUrl, String split) throws IOException {
         FileInputStream inputStream = new FileInputStream(fileUrl);
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        String str = null;
+        String str;
         StringBuilder stringBuilder = new StringBuilder();
         while((str = bufferedReader.readLine()) != null)
         {
             stringBuilder.append(str).append("\n");
         }
-        List<JSONObject> jsonObjectList = JsonUtils.getJsonObjects(stringBuilder.toString());
+        List<JSONObject> jsonObjectList = JsonUtils.getJsonObjects(stringBuilder.toString(),split);
         while (jsonObjectList.size()>0){
-            List<JSONObject> newJsonObjectList = JsonUtils.getJsonObjects(jsonObjectList);
+            List<JSONObject> newJsonObjectList = JsonUtils.getJsonObjects(jsonObjectList, split);
             jsonObjectList = newJsonObjectList;
         }
     }
@@ -44,33 +44,33 @@ public class JsonUtils {
      * @param str
      * @return
      */
-    public static List<JSONObject> getJsonObjects(String str){
+    public static List<JSONObject> getJsonObjects(String str, String split){
         Object object = new JSONTokener(str).nextValue();
         List<JSONObject> jsonObjectList = new ArrayList<>();
         if(object instanceof JSONObject){
             jsonObjectList.add((JSONObject)object);
         }else if (object instanceof String || object instanceof Integer || object instanceof ArrayList) {
-            sb.append(object).append(",");
+            sb.append(object).append(split);
         }else{
             JSONArray retArray = (JSONArray)object;
             retArray.forEach(retObject->{
                 if (retObject instanceof JSONObject){
                     jsonObjectList.add((JSONObject) retObject);
                 }else{
-                    sb.append(retObject).append(",");
+                    sb.append(retObject).append(split);
                 }
             });
         }
         return jsonObjectList;
     }
-    public static List<JSONObject> getJsonObjects(List<JSONObject> jsonObjectList){
+    public static List<JSONObject> getJsonObjects(List<JSONObject> jsonObjectList, String split){
         List<JSONObject> newJsonObjectList = new ArrayList<>();
         jsonObjectList.forEach(retJson->{
             Iterator<String> keys = retJson.keys();
             while (keys.hasNext()){
                 String key = String.valueOf(keys.next());
                 String value = retJson.get(key).toString();
-                List<JSONObject> cacheObjectList = getJsonObjects(value);
+                List<JSONObject> cacheObjectList = getJsonObjects(value, split);
                 newJsonObjectList.addAll(cacheObjectList);
             }
         });
--- src/main/java/com/wxscistor/jsonhandle/Test.java
+++ src/main/java/com/wxscistor/jsonhandle/Test.java
@@ -11,7 +11,7 @@ public class Test {
 
     public static void main(String[] args) throws IOException {
         String fileUrl = "/Users/liqijian/Desktop/1.json";
-        JsonUtils.jsonHandle(fileUrl);
+        JsonUtils.jsonHandle(fileUrl, ",");
         System.out.println(JsonUtils.sb);
     }
 }
